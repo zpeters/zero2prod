e1e6fd5400e81fd72229435dea531f74c733c7e3
Zach Peters <zpeters@gmail.com>
2022-01-06 07:29:44 -0600

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn health_check() -> impl Responder {
+    println!("/health_request ping");
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    println!("Listening on port 8000");
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
     })
     .bind("127.0.0.1:8000")?
     .run()
