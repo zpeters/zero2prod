7f9239be70c92989b3feb3dff1b9043dcda8db28
Zach Peters <zpeters@gmail.com>
2022-01-25 16:51:48 -0600

    Starting on email client

---
+++ src/email_client.rs
@@ -0,0 +1,26 @@
+use crate::domain::SubscriberEmail;
+
+pub struct EmailClient {
+    http_client: Client,
+    base_url: String,
+    sender: SubscriberEmail,
+}
+
+impl EmailClient {
+    pub new(base_url: String, sender: SubscriberEmail) -> Self {
+        Self{
+            http_client: Client::new(),
+            base_url,
+            sender
+        }
+    }
+    pub async fn send_email(
+        &self,
+        recipient: SubscriberEmail,
+        subject: &str,
+        html_content: &str,
+        text_content: &str,
+    ) -> Result<(), String> {
+        todo!()
+    }
+}
